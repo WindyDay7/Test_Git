2defde316d29aaf10a05d54ea48dc461aed8b67f
WindyDay <wufeng@WindyDaydeMacBook-Pro.local>
2023-01-29 22:23:59 +0900

    new branch2

--- file_a.cpp
+++ file_a.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main(int argc, char const* argv[])
 {
     cout << "this is file a" << endl;
-    cout << "This is file A" << endl;
+    cout << "This is file B" << endl;
+    cout << "This is file C" << endl;
     return 0;
 }
