22531901dfb633eaa7ecfe26eac8cc19777c7c81
虾野百鹤 <37218404+WindyDay7@users.noreply.github.com>
2023-01-29 21:23:10 +0900

    Update file_a.cpp
    This change

--- file_a.cpp
+++ file_a.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main(int argc, char const* argv[])
 {
     cout << "this is file a" << endl;
-    cout << "This is file C" << ennl;
+    cout << "This is file C" << endl;
     return 0;
 }
